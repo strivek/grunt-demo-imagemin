dbd727038b5ed8290923d8ee02d0df8e72eeb767
GaoFei <strivek@163.com>
2015-01-16 12:02:52 +0800

    添加demo

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+    grunt.initConfig({
+        imagemin: {
+            test: {
+            	options:{
+            		optimizationLevel:6 
+            	},
+                files:[{
+                    expand: true, //是否可以自动创建文件夹
+                    cwd: "src/images",
+                    src: "*.{gif,GIF,jpg,JPG,png,PNG}",
+                    dest: "dist/images"
+                }]
+            }
+        }
+    })
+    grunt.registerTask("test",['imagemin:test']);
+
+}
